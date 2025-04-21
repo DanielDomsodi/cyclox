712bd691180331b99ffbb9abca21e914e9dcde28
Daniel Domsodi <dev.daniel.d@gmail.com>
2025-04-21 15:44:30 +0200

    feat(analytics): integrate Vercel Speed Insights and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 import Header from './Header';
 import { SWRProvider } from '@/components/SWRProvider';
-import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -33,6 +34,7 @@ export default function RootLayout({
         <SWRProvider>{children}</SWRProvider>
         <Header />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
